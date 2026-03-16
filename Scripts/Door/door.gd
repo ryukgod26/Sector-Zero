3e42b5c8f5576be2ca3ed509dd3fc995f88d9297
raviwhitehat <ekravi26@gmail.com>
2026-03-16 17:54:01 +0530

    Refactor DoorComponent to support multiple door types and remove redundant rotating door script

--- Scripts/Door/door.gd
+++ Scripts/Door/door.gd
@@ -1,19 +1,28 @@
 class_name DoorComponent
 extends Node
 
-@export var direction: Vector3
+enum DoorType {Sliding, Rotating}
+
+@export_group("Door Settings")
+@export var door_type: DoorType
+@export var move_dir: Vector3
+@export var rotation := Vector3(0,1,0)
+@export var rotation_amount := 90.
 @export var door_size: Vector3
-@export var speed := 0.5
 @export var close_time := 2.0
+@export_group("Tween Setings")
+@export var speed := 0.5
 @export var transition_type: Tween.TransitionType
 @export var easing: Tween.EaseType
 
 var parent
 var orig_pos: Vector3
+var orig_rot: Vector3
 
 func _ready() -> void:
 	parent = get_parent()
 	orig_pos = parent.position
+	orig_rot = parent.rotation
 	parent.ready.connect(connect_parent)
 
 func connect_parent():
@@ -21,10 +30,19 @@ func connect_parent():
 
 func open_door() -> void:
 	var tween = create_tween()
-	tween.tween_property(parent,"position",orig_pos + (direction * door_size),speed).set_trans(transition_type).set_ease(easing)
+	match door_type:
+		DoorType.Sliding:
+			tween.tween_property(parent,"position",orig_pos + (move_dir * door_size),speed).set_trans(transition_type).set_ease(easing)
+		DoorType.Rotating:
+			#tween.tween_property(parent,"position",orig_pos + (move_dir * door_size),speed).set_trans(transition_type).set_ease(easing)
+			tween.tween_property(parent,"rotation",orig_rot + (rotation * deg_to_rad(rotation_amount)),speed).set_trans(transition_type).set_ease(easing)
 	tween.tween_interval(close_time)
 	tween.tween_callback(close_door)
 
 func close_door() -> void:
 	var tween = create_tween()
-	tween.tween_property(parent,"position",orig_pos,speed).set_trans(transition_type).set_ease(easing)
+	match door_type:
+		DoorType.Sliding:
+			tween.tween_property(parent,"position",orig_pos,speed).set_trans(transition_type).set_ease(easing)
+		DoorType.Rotating:
+			tween.tween_property(parent,"rotation",orig_rot,speed).set_trans(transition_type).set_ease(easing)
